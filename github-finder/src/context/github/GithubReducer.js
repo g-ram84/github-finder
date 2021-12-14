4a7ba06bb3248085335ac6ea7ed4bc7406aa67b0
Graeme Chalmers <chalmers.graeme@gmail.com>
2021-12-14 15:18:16 -0800

    clean up

--- github-finder/src/context/github/GithubReducer.js
+++ github-finder/src/context/github/GithubReducer.js
@@ -16,12 +16,6 @@ const githubReducer = (state, action) => {
 				...state,
 				users: []
 			};
-		case "GET_USER":
-			return {
-				...state,
-				user: action.payload,
-				loading: false
-			};
 		case "GET_USER_AND_REPOS":
 			return {
 				...state,
