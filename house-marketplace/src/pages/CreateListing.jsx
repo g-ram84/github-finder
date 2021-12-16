638ce91d1c57d1a37c5355f1df2448ed9d5d35c8
Graeme Chalmers <chalmers.graeme@gmail.com>
2021-12-15 16:07:47 -0800

    added geolocation

--- house-marketplace/src/pages/CreateListing.jsx
+++ house-marketplace/src/pages/CreateListing.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import Spinner from "../components/Spinner";
 
 function CreateListing() {
@@ -33,7 +34,7 @@ function CreateListing() {
 		offer,
 		regularPrice,
 		discountedPrice,
-		longitide,
+		longitude,
 		images,
 		latitude
 	} = formData;
@@ -59,9 +60,47 @@ function CreateListing() {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [isMounted]);
 
-	const onSubmit = (e) => {
+	const onSubmit = async (e) => {
 		e.preventDefault();
-		console.log(formData);
+		setLoading(true);
+
+		if (discountedPrice >= regularPrice) {
+			setLoading(false);
+			toast.error("Discounted price must be less than regular price");
+			return;
+		}
+		if (images.length > 6) {
+			setLoading(false);
+			toast.error("Max 6 images");
+			return;
+		}
+		let geolocation = {};
+		let location;
+
+		if (geolocationEnabled) {
+			const response = await fetch(
+				`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+			);
+			const data = await response.json();
+			geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+			geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+
+			location =
+				data.status === "ZERO_RESULTS"
+					? undefined
+					: data.results[0]?.formatted_address;
+
+			if (location === undefined || location.includes("undefined")) {
+				setLoading(false);
+				toast.error("Please enter a correct address");
+				return;
+			}
+		} else {
+			geolocation.lat = latitude;
+			geolocation.lng = longitude;
+			location = address;
+		}
+		setLoading(false);
 	};
 
 	const onMutate = (e) => {
@@ -239,7 +278,7 @@ function CreateListing() {
 								<input
 									type="number"
 									id="longitide"
-									value={longitide}
+									value={longitude}
 									onChange={onMutate}
 									className="formInputSmall"
 									required
