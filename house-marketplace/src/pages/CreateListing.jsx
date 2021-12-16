aae50d274f5f6d6d85b3c47b653fbbce67604cb8
Graeme Chalmers <chalmers.graeme@gmail.com>
2021-12-15 17:19:40 -0800

    Added code to save files to db

--- house-marketplace/src/pages/CreateListing.jsx
+++ house-marketplace/src/pages/CreateListing.jsx
@@ -1,7 +1,15 @@
 import { useState, useEffect, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+	getStorage,
+	ref,
+	uploadBytesResumable,
+	getDownloadURL
+} from "firebase/storage";
+import { db } from "../firebase.config";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { v4 as uuidv4 } from "uuid";
 import Spinner from "../components/Spinner";
 
 function CreateListing() {
@@ -100,6 +108,44 @@ function CreateListing() {
 			geolocation.lng = longitude;
 			location = address;
 		}
+
+		const storeImage = async (image) => {
+			return new Promise((resolve, reject) => {
+				const storage = getStorage();
+				const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+
+				const storageRef = ref(storage, "images/" + fileName);
+
+				const uploadTask = uploadBytesResumable(storageRef, image);
+
+				uploadTask.on(
+					"state_changed",
+					(snapshot) => {
+						const progress =
+							(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+						console.log("Upload is " + progress + "% done");
+					},
+					(error) => {
+						reject(error);
+					},
+					() => {
+						getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+							resolve(downloadURL);
+						});
+					}
+				);
+			});
+		};
+		const imgUrls = await Promise.all(
+			[...images].map((image) => storeImage(image))
+		).catch(() => {
+			setLoading(false);
+			toast.error("Images not uploaded");
+			return;
+		});
+
+		console.log(imgUrls);
+
 		setLoading(false);
 	};
 
@@ -265,22 +311,22 @@ function CreateListing() {
 							<div>
 								<label className="formLabel">Latitude</label>
 								<input
+									className="formInputSmall"
 									type="number"
 									id="latitude"
 									value={latitude}
 									onChange={onMutate}
-									className="formInputSmall"
 									required
 								/>
 							</div>
 							<div>
 								<label className="formLabel">Longitide</label>
 								<input
+									className="formInputSmall"
 									type="number"
 									id="longitide"
 									value={longitude}
 									onChange={onMutate}
-									className="formInputSmall"
 									required
 								/>
 							</div>
